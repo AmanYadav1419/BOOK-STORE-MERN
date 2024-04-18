21f624fa7b9586afd6178fe3ab3d62da32cbd5c7
unknown <amanyadav16544@gmail.com>
2024-04-18 20:54:17 +0530

    Creating first Http Route

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("welcome to the MERN Stack Book Project");
+});
+
 app.listen(PORT, () => {
   console.log(`App is running on Port: ${PORT}`);
 });
