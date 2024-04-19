3c0e3a0b25c44d22766593e56ed2579d783e0710
unknown <amanyadav16544@gmail.com>
2024-04-19 09:22:33 +0530

    Save a new Book with mongoose

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 import express from "express";
 import { PORT, mongoDBURl } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+
+// Middlewre for parsing request body
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("welcome to the MERN Stack Book Project");
+});
+
+// Route for Save a new Book
+app.post("/book", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required feilds: title,author,publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(234).send(book);
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
