b4e0dc09a34ece69353c4d1cc0ab6865f7c633ad
unknown <amanyadav16544@gmail.com>
2024-04-21 14:33:23 +0530

    CORS policy in Node js and Express js

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ import express from "express";
 import { PORT, mongoDBURl } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
-import bookRoutes from './routes/BooksRoutes.js';
-
+import bookRoutes from "./routes/BooksRoutes.js";
+import cors from 'cors';
 const app = express();
 
-// Middlewre for parsing request body
+// Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origin with default of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: "https://ocalhost:3000",
+    method: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("welcome to the MERN Stack Book Project");
 });
 
-app.use('/book', bookRoutes);
+app.use("/book", bookRoutes);
 
 mongoose
   .connect(mongoDBURl)
