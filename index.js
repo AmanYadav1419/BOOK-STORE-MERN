9418404cbf8a12813467ddf4569ea1318193b897
unknown <amanyadav16544@gmail.com>
2024-04-29 23:50:32 +0530

    changes & advancement in backend

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PORT, mongoDBURL } from './config.js';
+import { PORT, mongoDBURl } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
@@ -29,7 +29,7 @@ app.get('/', (request, response) => {
 app.use('/books', booksRoute);
 
 mongoose
-  .connect(mongoDBURL)
+  .connect(mongoDBURl)
   .then(() => {
     console.log('App connected to database');
     app.listen(PORT, () => {
